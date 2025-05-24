bd5dab07ea348f967f0a042ce376b9c3cc07d669
Ahmedzzzzzzz <ahmedfun450@gmail.com>
2025-05-24 20:07:49 +0300

    Update projectofsquarenum.py

--- projectofsquarenum.py
+++ projectofsquarenum.py
@@ -1,7 +1,7 @@
 start = int(input("enter start number: "))
 end = int(input("enter end number: "))
 
-for num in range(1, 2 + 1):
+for num in range(start, end+ 1):
     square = num * num
     if square % 2 == 0:
         print(square, "is even")
